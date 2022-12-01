50b18906578b87fdfad609a413341f4dfb321549
pow <vkflskfdk1@naver.com>
2022-12-01 18:06:18 +0900

    게임 중 숫자 버튼 클릭 시 버튼에 해당하는 점수 초기화 완료

--- app/src/main/java/com/brownik/simplegame/ButtonStateViewModel.kt
+++ app/src/main/java/com/brownik/simplegame/ButtonStateViewModel.kt
@@ -17,11 +17,12 @@ class ButtonStateViewModel : ViewModel() {
         _buttonStateData.value = MutableList(9) { 0 }.toList()
     }
 
-    fun initButtonBackground(){
+    fun initButtonBackground() {
         _buttonStateData.postValue(MutableList(9) { 0 }.toList())
     }
 
     private fun makeRandomPosition(): Int = (0..8).random()
+
     private fun makeRandomScore(): Int = makeRandomScoreList().random()
 
     // 버튼 상태 바꾸기
@@ -51,21 +52,31 @@ class ButtonStateViewModel : ViewModel() {
             5 -> +5
             else -> 0
         }
+        initOneButton(position)
         return score
+    }
+
+    private fun initOneButton(position: Int) {
+        MyObject.makeLog("initOneButton")
+        val list = _buttonStateData.value?.toMutableList()
+        list?.let {
+            it[position] = 0
+        }
+        _buttonStateData.postValue(list?.toList())
     }
 
     private fun makeRandomScoreList(): List<Int> {
         val list = mutableListOf<Int>()
-        list.addAll(MutableList(1){ -5 })
-        list.addAll(MutableList(4){ -4 })
-        list.addAll(MutableList(10){ -3 })
-        list.addAll(MutableList(15){ -2 })
-        list.addAll(MutableList(20){ -1 })
-        list.addAll(MutableList(20){ 1 })
-        list.addAll(MutableList(15){ 2 })
-        list.addAll(MutableList(10){ 3 })
-        list.addAll(MutableList(4){ 4 })
-        list.addAll(MutableList(1){ 5 })
+        list.addAll(MutableList(1) { -5 })
+        list.addAll(MutableList(4) { -4 })
+        list.addAll(MutableList(10) { -3 })
+        list.addAll(MutableList(15) { -2 })
+        list.addAll(MutableList(20) { -1 })
+        list.addAll(MutableList(20) { 1 })
+        list.addAll(MutableList(15) { 2 })
+        list.addAll(MutableList(10) { 3 })
+        list.addAll(MutableList(4) { 4 })
+        list.addAll(MutableList(1) { 5 })
         return list.shuffled().toList()
     }
 }
